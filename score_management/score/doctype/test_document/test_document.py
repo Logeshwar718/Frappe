c5590f47e935d98dd96dd88697acd82935c1585e
logeshwarks <logeshwarks2005@gmail.com>
2026-08-03 17:31:10 +0530

    Update score management code

--- score_management/score/doctype/test_document/test_document.py
+++ score_management/score/doctype/test_document/test_document.py
@@ -3,8 +3,37 @@
 
 import frappe
 from frappe.model.document import Document
-
+from frappe.model.naming import make_autoname
 
 class Test_Document(Document):
 	def before_save(self):
-		self.description="Default Description"
+		if not self.description:
+			self.description="Default Description"
+
+	#controller hooks
+
+	def before_insert(self):
+		if not self.age:
+			self.age=20
+		frappe.msgprint("before_inserted hook executed")
+
+	def before_naming(self):
+		if not self.department:
+			self.department="CSE"
+		frappe.msgprint("before_naming hook executed")
+	
+	def autoname(self):
+		self.name= make_autoname(f"{self.department}-.####")
+
+	def before_validate(self):
+		if not self.name1:
+			self.name1="random_user"
+		frappe.msgprint("before_validate hook executed")
+	
+	def validate(self):
+		if self.age<18:
+			frappe.throw("Age must be greater than 18")
+		frappe.msgprint("validate hook executed")
+
+	def after_insert(self):
+		frappe.msgprint("Record inserted")
